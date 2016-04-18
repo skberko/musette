2d1a24ff731269f76ff7bf03e63bfc78f765dd64
Sam Berkowitz <skberkowitz@gmail.com>
2016-04-18 15:54:02 -0700

    completed npm config, created empty staticpagescontroller

---
+++ frontend/musette.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var MyComponent = React.createClass({
+  render: function () {
+    return(
+      <div>Hello, Musette!</div>
+    );
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+});
