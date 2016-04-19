33dd09c4088a65ea67806d8cd2fd93afbdbbf552
Sam Berkowitz <skberkowitz@gmail.com>
2016-04-18 23:21:01 -0700

    built activitiesIndex react component, now need to render it in musette root

--- frontend/musette.jsx
+++ frontend/musette.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var ApiUtil = require('./util/apiUtil') // can be removed once flux cycle is in place
 var Dispatcher = require('./dispatcher/dispatcher')
 var ActivityStore = require('./stores/activityStore')
+var ActivitiesIndex = require('./components/activities/activitiesIndex.jsx');
 
 var Musette = React.createClass({
   render: function () {
