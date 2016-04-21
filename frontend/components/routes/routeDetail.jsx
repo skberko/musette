07b0a32e560c0ca367f42a1d07a46c786257e926
Sam Berkowitz <skberkowitz@gmail.com>
2016-04-21 16:58:24 -0700

    remove stray debuggers

--- frontend/components/routes/routeDetail.jsx
+++ frontend/components/routes/routeDetail.jsx
@@ -16,7 +16,6 @@ var RouteDetail = React.createClass({
   // will this result in unnecessary additional api call upon minor changes
   // to the RouteDetail component? something to think about
   _onChange: function () {
-    debugger
     this.setState({ routeDetail: RouteDetailStore.grabRouteDetail() });
   },
 
