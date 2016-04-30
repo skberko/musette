def5d00dd5bb03c9c3c38b647df2c83e57523616
Sam Berkowitz <skberkowitz@gmail.com>
2016-04-29 17:08:04 -0700

    remove some old commented out code

--- frontend/components/routes/routeDetailStopsListGroup.jsx
+++ frontend/components/routes/routeDetailStopsListGroup.jsx
@@ -7,16 +7,11 @@ var RouteDetailStopsListGroup = React.createClass({
     return { places: PlaceStore.all() };
   },
 
+  convertMetersToMiles: function (distanceInMeters) {
+    return Number(distanceInMeters) / 1609.344
+  },
+
   render: function () {
-    // if (this.props.places.length === 0) {
-    //   return(
-    //     <li>
-    //       <h5>Stop #{this.props.stopGroupId + 1}:</h5>
-    //       <div>Sorry, there do not appear to be any stopping points here.</div>
-    //     </li>
-    //   )
-    // }
-    // debugger
     if (this.props.stopGroup.places.length === 0) {
       return(
         <div>
@@ -34,38 +29,6 @@ var RouteDetailStopsListGroup = React.createClass({
         })}
       </div>
     )
-
-    // <h5>Stop #{this.props.stopGroup.stopGroupId + 1}:</h5>
-    // <ul>{this.props.stopGroup.places.map(function (stopGroupPlace) {
-    //   return(<RouteDetailStopsListGroupItem
-    //         key={stopGroupPlace.id}
-    //         name={stopGroupPlace.name}
-    //         location={stopGroupPlace.geometry.location}
-    //         lat={stopGroupPlace.geometry.location.lat()}
-    //         lng={stopGroupPlace.geometry.location.lng()}
-    //         types={stopGroupPlace.types}
-    //         vicinity={stopGroupPlace.stopGroupId}
-    //         rating={stopGroupPlace.rating}/>);
-    //   })}
-    // </ul>
-
-    // return(
-    //   <Tab>
-    //     <h5>Stop #{this.props.stopGroupId + 1}:</h5>
-    //       <ul>{this.props.places.map(function (stopGroupPlace) {
-    //           return(<RouteDetailStopsListGroupItem
-    //                 key={stopGroupPlace.id}
-    //                 name={stopGroupPlace.name}
-    //                 location={stopGroupPlace.geometry.location}
-    //                 lat={stopGroupPlace.geometry.location.lat()}
-    //                 lng={stopGroupPlace.geometry.location.lng()}
-    //                 types={stopGroupPlace.types}
-    //                 vicinity={stopGroupPlace.stopGroupId}
-    //                 rating={stopGroupPlace.rating}/>);
-    //           })}
-    //       </ul>
-    //   </Tab>
-    // );
   }
 })
 
