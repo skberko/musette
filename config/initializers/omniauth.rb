768b9dde712d1da6a02a878ef65f807a7888332a
Sam Berkowitz <skberkowitz@gmail.com>
2016-05-12 16:16:35 -0700

    change strava v3 api oauth scope to public from private

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 # SKB: changed scope below to 'view_private', but defaults
 # is 'public'
-  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], scope: 'view_private'
+  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], scope: 'public'
 end
